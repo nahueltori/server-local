74bade19868f3b69ccd95e69e8b0fb84639af102
Nahuel Tori <nahueltori@gmail.com>
2026-08-27 15:25:40 -0300

    Immich install

---
+++ immich/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Install script for local Immich server
+sudo apt update
+sudo apt install -y ca-certificates curl
+
+sudo install -m 0755 -d /etc/apt/keyrings
+
+sudo curl -fsSL \
+  https://download.docker.com/linux/ubuntu/gpg \
+  -o /etc/apt/keyrings/docker.asc
+
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo tee /etc/apt/sources.list.d/docker.sources > /dev/null <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+sudo apt install -y \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
+  docker-buildx-plugin \
+  docker-compose-plugin
+
+wget -O docker-compose.yml \
+  https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml
+
+wget -O .env \
+  https://github.com/immich-app/immich/releases/latest/download/example.env
